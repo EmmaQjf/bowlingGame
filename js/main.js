da9767459d73063b513b32666577d01ac8c2b0d4
junfengqiao <jfqemma@gmail.com>
2023-11-30 17:02:15 -0500

    add scoreboard

--- js/main.js
+++ js/main.js
@@ -11,6 +11,32 @@
 
 
 	/*----- functions -----*/
+ let count = 12;
+ let round = Math.floor((count-1)/2);
+ console.log(round);
+ function firstOrSecondTry() {
+ if (count % 2 === 0) {
+	return 1;
+ } else {
+	return 0;
+ }
+}
+
+let whichtry = firstOrSecondTry();
+// let throw = whichtry();
+console.log(whichtry);
+
+let eachThrowCellID = `r2c${round}throw${whichtry}`;
+console.log(eachThrowCellID);
+const cellEl = document.getElementById(eachThrowCellID);
+cellEl.innerHTML = "88";
+
+let eachroundSum = `r3c${round}`;
+const eachroundSumEl = document.getElementById(eachroundSum);
+eachroundSumEl.innerHTML = "0";
+console.log(eachroundSum);
+
+
 
 
     // pseudocode 
